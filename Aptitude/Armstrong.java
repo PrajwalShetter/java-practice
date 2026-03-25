5368d3b74074554d2189f05a7a6ac781a29da351
prajwal shetter <198955243+PrajwalShetter@users.noreply.github.com>
2026-03-25 15:19:53 +0530

    Aptitude Programs

---
+++ Aptitude/Armstrong.java
@@ -0,0 +1,16 @@
+class Armstrong
+{
+	public static void main(String[] args){
+		
+		int num = 153;
+		int count =0
+		int rem =0;
+		while(num>0){
+			num = num%10;
+			count++;
+		}
+		int reminder = num % 10*count;
+		
+	
+	
+}
