8f8da1ca4cbf98cbb1eeb9b10d24d8c6e0428e01
prajwal shetter <198955243+PrajwalShetter@users.noreply.github.com>
2026-04-12 23:17:44 +0530

    Declaring methods and variables in class -- practice programs

---
+++ Methods/TrainRunner.java
@@ -0,0 +1,21 @@
+class TrainRunner{
+
+	public static void main(String[] args)
+	{
+	
+		Train.start();
+		Train.start();
+		Train.start();
+		
+		Train.stop();
+		Train.stop();
+		Train.stop();
+		
+		Train.speed();
+		Train.speed();
+		Train.speed();
+		
+		
+		
+	}
+}
