0ca6653decd07dd87c4bb5595a41cfc525bb2454
prajwal shetter <198955243+PrajwalShetter@users.noreply.github.com>
2026-04-12 23:20:17 +0530

    Declaring methods and variables in class -- practice programs

---
+++ Methods/Street.java
@@ -0,0 +1,11 @@
+class Street{
+
+	static void details(){
+		System.out.println("Street name: MG Road");
+		pincode();
+	}
+	
+	static void pincode(){
+		System.out.println("Pincode: 583231");
+	}
+}
