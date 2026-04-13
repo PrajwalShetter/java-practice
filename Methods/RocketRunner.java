61efa915cbc8e9314e0992da7b5a8145871f9f93
prajwal shetter <198955243+PrajwalShetter@users.noreply.github.com>
2026-04-13 23:07:36 +0530

    Declaring methods and variables in class -- practice programs

---
+++ Methods/RocketRunner.java
@@ -0,0 +1,24 @@
+class RocketRunner{
+
+public static void main(String[] args){
+ 
+ Rocket.launch();
+ Rocket.launch();
+ Rocket.launch();
+ 
+ Rocket.fly();
+ Rocket.fly();
+ Rocket.fly();
+ 
+ Rocket.fuel();
+ Rocket.fuel();
+ Rocket.fuel();
+ }
+ }
+ 
+ 
+ 
+ 
+ 
+ 
+ 
