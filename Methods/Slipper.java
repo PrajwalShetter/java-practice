cb3343d041d8ae8b27025b128e4408e659bd7d46
prajwal shetter <198955243+PrajwalShetter@users.noreply.github.com>
2026-04-13 23:08:07 +0530

    Declaring methods and variables in class -- practice programs

---
+++ Methods/Slipper.java
@@ -0,0 +1,23 @@
+class Slipper {
+    static void wear(){
+		System.out.println("Slipper is worn on feet"); 
+		}
+		
+    static void walk(){
+		System.out.println("Walking with slipper");
+		}
+		
+    static void comfort(){
+		System.out.println("Slipper gives comfort"); 
+		clean();
+		protect();
+		}
+		
+    static void clean(){
+		System.out.println("Cleaning slipper");
+		}
+		
+    static void protect(){
+		System.out.println("Slipper protects feet"); 
+		}
+}
