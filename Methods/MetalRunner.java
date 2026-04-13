b62fdd0f27c47787779b03a1334715917b22e171
prajwal shetter <198955243+PrajwalShetter@users.noreply.github.com>
2026-04-13 23:09:17 +0530

    Declaring methods and variables in class -- practice programs

---
+++ Methods/MetalRunner.java
@@ -0,0 +1,23 @@
+class MetalRunner{
+
+	public static void main(String[] args){
+	
+		Metal.melt();
+		Metal.melt();
+		Metal.melt();
+		
+		Metal.conduct();
+		Metal.conduct();
+		Metal.conduct();
+		
+		Metal.Strong();
+		Metal.Strong();
+		Metal.Strong();
+		
+		
+		
+	
+	}
+
+
+}
