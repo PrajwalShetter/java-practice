7062af64e91cca55d726cbd6fd4c04a484250e8b
prajwal shetter <198955243+PrajwalShetter@users.noreply.github.com>
2026-04-13 23:05:56 +0530

    Declaring methods and variables in class -- practice programs

---
+++ Methods/SnakeRunner.java
@@ -0,0 +1,23 @@
+class SnakeRunner{
+
+	public static void main(String[] args){
+	
+	Snake.crawl();
+	Snake.crawl();
+	Snake.crawl();
+	
+	Snake.hiss();
+	Snake.hiss();
+	Snake.hiss();
+	
+	Snake.bite();
+	Snake.bite();
+	Snake.bite();
+	}
+	}
+	
+	
+	
+	
+	
+	
