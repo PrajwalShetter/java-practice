b587bec0a76a04dc82d1254cbd0d6c8e6fc55efb
prajwal shetter <198955243+PrajwalShetter@users.noreply.github.com>
2026-04-13 23:09:33 +0530

    Declaring methods and variables in class -- practice programs

---
+++ Methods/Metal.java
@@ -0,0 +1,24 @@
+class Metal{
+	
+	static void melt(){
+		System.out.println("Heat the Metal, metal become melt");
+	}
+	
+	static void conduct(){
+		System.out.println("Metal conduct electricity");
+	}
+	
+	static void Strong(){
+		System.out.println("Metal is Strong");
+		recycle();
+		shine();
+	}
+	
+	static void recycle(){
+		System.out.println("Metal can be recycled");
+	}
+	
+	static void shine(){
+		System.out.println("Metal Shines");
+	}
+}
