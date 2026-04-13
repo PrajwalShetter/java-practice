fbff3713cd852a8f6e4103df89baf1a66d653d30
prajwal shetter <198955243+PrajwalShetter@users.noreply.github.com>
2026-04-13 23:06:37 +0530

    Declaring methods and variables in class -- practice programs

---
+++ Methods/SlipperRunnere.java
@@ -0,0 +1,22 @@
+class SlipperRunnere{
+	
+public static void main(String[] args){
+	
+	Slipper.wear();
+	Slipper.wear();
+	Slipper.wear();
+	
+	Slipper.walk();
+	Slipper.walk();
+	Slipper.walk();
+	
+	Slipper.comfort();
+	Slipper.comfort();
+	Slipper.comfort();
+}
+}
+
+	
+	
+	
+	
