540f058d836e4ae88e48c84e4f65283857b0475f
prajwal shetter <198955243+PrajwalShetter@users.noreply.github.com>
2026-04-13 23:08:41 +0530

    Declaring methods and variables in class -- practice programs

---
+++ Methods/RoadRunner.java
@@ -0,0 +1,18 @@
+class RoadRunner{
+	
+	public static void main(String[] args){
+		
+		Road.travel();
+		Road.travel();
+		Road.travel();
+		
+		Road.traffic();
+		Road.traffic();
+		Road.traffic();
+		
+		Road.repair();
+		Road.repair();
+		Road.repair();
+}
+}
+		
