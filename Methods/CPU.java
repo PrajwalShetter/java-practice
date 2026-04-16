6858b040cf7fc68816bf90dc965aa20906eec188
prajwal shetter <198955243+PrajwalShetter@users.noreply.github.com>
2026-04-16 23:29:11 +0530

    Declaring methods and variables in class -- practice programs

---
+++ Methods/CPU.java
@@ -0,0 +1,23 @@
+class CPU {
+    static void process(){
+		System.out.println("CPU processes instructions"); 
+		}
+		
+    static void calculate(){
+		System.out.println("CPU performs calculations");
+		}
+		
+    static void control(){
+		System.out.println("CPU controls computer operations"); 
+		speed();
+		execute();
+		}
+		
+    static void speed(){
+		System.out.println("CPU determines system speed"); 
+		}
+		
+    static void execute(){ 
+		System.out.println("CPU executes programs");
+		}
+}
