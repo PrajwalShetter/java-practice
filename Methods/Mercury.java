2b26938fc481cfafc5603aeec8f47a4d79240a51
prajwal shetter <198955243+PrajwalShetter@users.noreply.github.com>
2026-04-16 23:30:45 +0530

    Declaring methods and variables in class -- practice programs

---
+++ Methods/Mercury.java
@@ -0,0 +1,24 @@
+class Mercury {
+	
+    static void planet(){
+		System.out.println("Mercury is a planet"); 
+		}
+		
+    static void hot(){ 
+	System.out.println("Mercury is very hot"); 
+	}
+	
+    static void orbit(){
+		System.out.println("Mercury orbits the Sun"); 
+		small();
+		rocky();
+		}
+		
+    static void small(){
+		System.out.println("Mercury is the smallest planet");
+		}
+		
+    static void rocky(){
+		System.out.println("Mercury is a rocky planet"); 
+		}
+}
