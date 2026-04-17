e6eec8ae4fc8869aad6e6bfa7c090f29eac0335c
prajwal shetter <198955243+PrajwalShetter@users.noreply.github.com>
2026-04-17 23:34:12 +0530

    Declaring methods and variables in class -- practice programs

---
+++ Methods/HardDisk.java
@@ -0,0 +1,25 @@
+class HardDisk {
+	
+    static void store(){
+		System.out.println("HardDisk stores data"); 
+		}
+		
+    static void read(){ 
+		System.out.println("HardDisk reads data");
+		}
+		
+    static void write(){
+		System.out.println("HardDisk writes data"); 
+		capacity();
+		backup();
+		
+		}
+		
+    static void capacity(){
+		System.out.println("HardDisk has storage capacity");
+		}
+		
+    static void backup(){
+		System.out.println("HardDisk used for backup"); 
+		}
+}
