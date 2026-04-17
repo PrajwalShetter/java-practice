27adc5b151c4b1036b11e947605782cf3196cc63
prajwal shetter <198955243+PrajwalShetter@users.noreply.github.com>
2026-04-17 23:32:36 +0530

    Declaring methods and variables in class -- practice programs

---
+++ Methods/District.java
@@ -0,0 +1,32 @@
+class District{
+	
+	static void run(){
+		System.out.println("I am from Koppal District");
+		System.out.println("District details are below");
+		taxing();
+		measure();
+		collect();
+		revenue();
+		budget();
+	}
+	
+	static void taxing(){
+		System.out.println("Koppal District collects upto 15000 crore of Tax per Year");
+	}
+	
+	static void measure(){
+		System.out.println("Koppal District area and development is measured by the government");
+	}
+	
+	static void collect(){
+		System.out.println("Taxes and revenues are collected from industries and citizens");
+	}
+	
+	static void revenue(){
+		System.out.println("District revenue helps in development and welfare programs");
+	}
+	
+	static void budget(){
+		System.out.println("District budget is allocated for infrastructure and public services");
+	}
+}
