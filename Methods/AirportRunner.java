9784f70b9ea293bbac377b62a460b3cc4f4eb555
prajwal shetter <198955243+PrajwalShetter@users.noreply.github.com>
2026-04-17 23:28:28 +0530

    Declaring methods and variables in class -- practice programs

---
+++ Methods/AirportRunner.java
@@ -0,0 +1,20 @@
+class AirportRunner{
+	
+	public static void main(String[] args){
+		
+		Airport.flights();
+		Airport.flights();
+		Airport.flights();
+		
+		Airport.security();
+		Airport.security();
+		Airport.security();
+		
+		Airport.boarding();
+		Airport.boarding();
+		Airport.boarding();
+}}
+		
+		
+		
+		
