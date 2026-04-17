54e797f837cbe75defa0bdd4dcbad659af9a4c9d
prajwal shetter <198955243+PrajwalShetter@users.noreply.github.com>
2026-04-17 23:29:13 +0530

    Declaring methods and variables in class -- practice programs

---
+++ Methods/CPURunner.java
@@ -0,0 +1,20 @@
+class CPURunner {
+    public static void main(String[] args) {
+	
+	CPU.process(); 
+	CPU.process(); 
+	CPU.process();
+	
+	CPU.calculate();
+	CPU.calculate();
+	CPU.calculate();
+	
+	CPU.control();
+	CPU.control();
+	CPU.control();
+	
+	}
+}
+
+	
+	
