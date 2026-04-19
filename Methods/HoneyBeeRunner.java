a0c0ee6440465a9622fbe20981e468d8ad1ffefc
prajwal shetter <198955243+PrajwalShetter@users.noreply.github.com>
2026-04-19 22:51:18 +0530

    Declaring methods and variables in class -- practice programs

---
+++ Methods/HoneyBeeRunner.java
@@ -0,0 +1,20 @@
+class HoneyBeeRunner{
+ 
+ public static void main(String[] args){
+	 
+	 HoneyBee.fly();
+	 HoneyBee.fly();
+	 HoneyBee.fly();
+	 
+	 HoneyBee.collect();
+	 HoneyBee.collect();
+	 HoneyBee.collect();
+	 
+	 HoneyBee.makeHoney();
+	 HoneyBee.makeHoney();
+	 HoneyBee.makeHoney();
+	 
+	 
+ }
+}s
+	 
