f60a5abc7504fac80a1c332c88d27496d8e5fd06
prajwal shetter <198955243+PrajwalShetter@users.noreply.github.com>
2026-04-19 23:07:17 +0530

    Return type -- practice programs

---
+++ Return types/GoldRunner.java
@@ -0,0 +1,27 @@
+class GoldRunner{
+	
+	
+	public static void main(String[] args){
+		
+		Gold.GetPriceByName("necklaces");
+		Gold.GetPriceByName("rings");
+		Gold.GetPriceByName("bangles");
+		Gold.GetPriceByName("bracelets");
+		Gold.GetPriceByName("earrings");
+		Gold.GetPriceByName("chains");
+		Gold.GetPriceByName("Choker");
+		Gold.GetPriceByName("Jhumkas");
+		Gold.GetPriceByName("EarCuffs");
+		Gold.GetPriceByName("Kada");
+		Gold.GetPriceByName("Kundan");
+		Gold.GetPriceByName("Navaratna");
+		Gold.GetPriceByName("Armlet");
+		Gold.GetPriceByName("ToeRings");
+		Gold.GetPriceByName("RaniHaar");
+		Gold.GetPriceByName("HasliNecklace");
+		Gold.GetPriceByName("HaathPhool");
+		Gold.GetPriceByName("Mangalsutra");
+		
+		
+	}
+}
