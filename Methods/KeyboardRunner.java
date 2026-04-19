4726df89bdf66171699d556be67e9436692f2ea8
prajwal shetter <198955243+PrajwalShetter@users.noreply.github.com>
2026-04-19 22:51:51 +0530

    Declaring methods and variables in class -- practice programs

---
+++ Methods/KeyboardRunner.java
@@ -0,0 +1,18 @@
+class KeyboardRunner{
+	
+	public static void main(String[] args){
+		
+		Keyboard.type();
+		Keyboard.type();
+		Keyboard.type();
+		
+		Keyboard.press();
+		Keyboard.press();
+		Keyboard.press();
+		
+		Keyboard.connect();
+		Keyboard.connect();
+		Keyboard.connect();
+		
+	}
+}
