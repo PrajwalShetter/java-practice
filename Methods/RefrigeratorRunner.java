d87531a2fa0687d355c27fd685622e5dc2b21065
prajwal shetter <198955243+PrajwalShetter@users.noreply.github.com>
2026-04-19 22:53:09 +0530

    Declaring methods and variables in class -- practice programs

---
+++ Methods/RefrigeratorRunner.java
@@ -0,0 +1,20 @@
+class RefrigeratorRunner{
+
+	public static void main(String[] args){
+		
+		Refrigerator.cool();
+        Refrigerator.cool();
+        Refrigerator.cool();
+		
+		Refrigerator.storeFood();
+		Refrigerator.storeFood();
+		Refrigerator.storeFood();
+		
+		Refrigerator.makeIce();
+		Refrigerator.makeIce();
+		Refrigerator.makeIce();
+		
+	
+		
+}
+}
