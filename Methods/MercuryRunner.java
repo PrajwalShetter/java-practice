c8f058f575cf159eed03c1838540fc2fc9f48e3f
prajwal shetter <198955243+PrajwalShetter@users.noreply.github.com>
2026-04-19 22:52:17 +0530

    Declaring methods and variables in class -- practice programs

---
+++ Methods/MercuryRunner.java
@@ -0,0 +1,20 @@
+class MercuryRunner{
+
+public static void main(String[] args){
+	
+	Mercury.planet();
+	Mercury.planet();
+	Mercury.planet();
+	
+	Mercury.hot();
+	Mercury.hot();
+	Mercury.hot();
+	
+	Mercury.orbit();
+	Mercury.orbit();
+	Mercury.orbit();
+}
+}
+
+	
+	
