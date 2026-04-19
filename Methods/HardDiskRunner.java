f88402373a21b02acc84a4551b720b08fbe88b56
prajwal shetter <198955243+PrajwalShetter@users.noreply.github.com>
2026-04-19 22:50:24 +0530

    Declaring methods and variables in class -- practice programs

---
+++ Methods/HardDiskRunner.java
@@ -0,0 +1,19 @@
+class HardDiskRunner{
+
+public static void main(String[] args){
+	
+	HardDisk.store();
+	HardDisk.store();
+	HardDisk.store();
+	
+	HardDisk.read();
+	HardDisk.read();
+	HardDisk.read();
+	
+	HardDisk.write();
+	HardDisk.write();
+	HardDisk.write();
+	
+}}
+	
+	
