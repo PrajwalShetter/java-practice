e6990c2ef4da0e6450fa8096d651fb26ba87370a
prajwal shetter <198955243+PrajwalShetter@users.noreply.github.com>
2026-04-24 23:40:22 +0530

    Return type -- practice program

---
+++ Return types/FoodItemsRunner.java
@@ -0,0 +1,68 @@
+class FoodItemsRunner{
+	
+	public static void main(String[] args){
+		
+		double price = FoodItems.getPriceByItems("dosa");
+		System.out.println(price);
+		
+		price = FoodItems.getPriceByItems("idli");
+		System.out.println(price);
+		
+		price = FoodItems.getPriceByItems("upma");
+		System.out.println(price);
+		
+		price = FoodItems.getPriceByItems("vada");
+		System.out.println(price);
+		
+		price = FoodItems.getPriceByItems("puri");
+		System.out.println(price);
+		
+		price = FoodItems.getPriceByItems("pongal");
+		System.out.println(price);
+		
+		price = FoodItems.getPriceByItems("bonda");
+		System.out.println(price);
+		
+		price = FoodItems.getPriceByItems("chapati");
+		System.out.println(price);
+		
+		price = FoodItems.getPriceByItems("paratha");
+		System.out.println(price);
+		
+		price = FoodItems.getPriceByItems("rice");
+		System.out.println(price);
+		
+		price = FoodItems.getPriceByItems("meals");
+		System.out.println(price);
+		
+		price = FoodItems.getPriceByItems("curdRice");
+		System.out.println(price);
+		
+		price = FoodItems.getPriceByItems("lemonRice");
+		System.out.println(price);
+		
+		price = FoodItems.getPriceByItems("puliyogare");
+		System.out.println(price);
+		
+		price = FoodItems.getPriceByItems("bisibelebath");
+		System.out.println(price);
+		
+		price = FoodItems.getPriceByItems("vegBiryani");
+		System.out.println(price);
+		
+		price = FoodItems.getPriceByItems("friedRice");
+		System.out.println(price);
+		
+		price = FoodItems.getPriceByItems("noodles");
+		System.out.println(price);
+		
+		price = FoodItems.getPriceByItems("gobiManchurian");
+		System.out.println(price);
+		
+		price = FoodItems.getPriceByItems("paneerButterMasala");
+		System.out.println(price);
+		
+		price = FoodItems.getPriceByItems("pizza");
+		System.out.println(price);
+	}
+}
