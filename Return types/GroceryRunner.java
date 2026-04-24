b2608042475691e28b4de5753a9e7c31cbfdce60
prajwal shetter <198955243+PrajwalShetter@users.noreply.github.com>
2026-04-24 23:41:13 +0530

    Return type -- practice program

---
+++ Return types/GroceryRunner.java
@@ -0,0 +1,27 @@
+class GroceryRunner{
+
+	public static void main(String[] args){
+		
+	//teaPowder, sugar, peper, soap, paste, brush, pinuts, jagurry, biscuts, oil, jawar, Rice, wheat, coconut, honey, gee, butter
+	
+	Grocery.getPriceByName("teaPowder");
+	Grocery.getPriceByName("sugar");
+	Grocery.getPriceByName("peper");
+	Grocery.getPriceByName("soap");
+	Grocery.getPriceByName("paste");
+	Grocery.getPriceByName("brush");
+	Grocery.getPriceByName("pinuts");
+	Grocery.getPriceByName("jagurry");
+	Grocery.getPriceByName("biscuts");
+	Grocery.getPriceByName("oil");
+	Grocery.getPriceByName("jawar");
+	Grocery.getPriceByName("Rice");
+	Grocery.getPriceByName("wheat");
+	Grocery.getPriceByName("coconut");
+	Grocery.getPriceByName("honey");
+	Grocery.getPriceByName("gee");
+	Grocery.getPriceByName("butter");
+	Grocery.getPriceByName("Ragi");
+
+	}
+}
