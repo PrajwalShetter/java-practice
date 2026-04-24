254c3cdc0d90c84d82f0f4134b03ca13e3205d5f
prajwal shetter <198955243+PrajwalShetter@users.noreply.github.com>
2026-04-24 23:44:45 +0530

    Return type -- practice program

---
+++ Return types/MedicineRunner.java
@@ -0,0 +1,25 @@
+class MedicineRunner{
+
+	public static void main(String[] args){
+
+		Medicine.getPriceByName("paracetamol");
+		Medicine.getPriceByName("dolo");
+		Medicine.getPriceByName("crocin");
+		Medicine.getPriceByName("aspirin");
+		Medicine.getPriceByName("amoxicillin");
+		Medicine.getPriceByName("azithromycin");
+		Medicine.getPriceByName("cetirizine");
+		Medicine.getPriceByName("pantoprazole");
+		Medicine.getPriceByName("ibuprofen");
+		Medicine.getPriceByName("metformin");
+		Medicine.getPriceByName("ranitidine");
+		Medicine.getPriceByName("omeprazole");
+		Medicine.getPriceByName("insulin");
+		Medicine.getPriceByName("vitaminC");
+		Medicine.getPriceByName("zinc");
+		Medicine.getPriceByName("antacid");
+		Medicine.getPriceByName("ORS");
+		Medicine.getPriceByName("tablet");
+
+	}
+}
