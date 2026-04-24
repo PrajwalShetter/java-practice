aa20dd30f0bc9dfdf4ffddf7a697f05f502f5f4a
prajwal shetter <198955243+PrajwalShetter@users.noreply.github.com>
2026-04-24 23:43:14 +0530

    Return type -- practice program

---
+++ Return types/GstRunner.java
@@ -0,0 +1,25 @@
+class GstRunner{
+
+	public static void main(String[] args){
+
+		Gst.getRateByType("cgst");
+		Gst.getRateByType("sgst");
+		Gst.getRateByType("igst");
+		Gst.getRateByType("utgst");
+		Gst.getRateByType("luxuryTax");
+		Gst.getRateByType("foodItems");
+		Gst.getRateByType("electronics");
+		Gst.getRateByType("cars");
+		Gst.getRateByType("gold");
+		Gst.getRateByType("hotelRooms");
+		Gst.getRateByType("textiles");
+		Gst.getRateByType("cement");
+		Gst.getRateByType("soap");
+		Gst.getRateByType("toothpaste");
+		Gst.getRateByType("biscuit");
+		Gst.getRateByType("mobile");
+		Gst.getRateByType("insurance");
+		Gst.getRateByType("unknown");
+
+	}
+}
