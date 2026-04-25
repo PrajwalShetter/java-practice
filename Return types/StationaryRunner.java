35c1f91fa9b9417f660b0171180eab1820caaaa0
prajwal shetter <198955243+PrajwalShetter@users.noreply.github.com>
2026-04-25 23:11:09 +0530

    Return type -- practice program

---
+++ Return types/StationaryRunner.java
@@ -0,0 +1,25 @@
+class StationaryRunner{
+
+	public static void main(String[] args){
+
+		Stationary.getPriceByName("pen");
+		Stationary.getPriceByName("pencil");
+		Stationary.getPriceByName("eraser");
+		Stationary.getPriceByName("sharpener");
+		Stationary.getPriceByName("notebook");
+		Stationary.getPriceByName("scale");
+		Stationary.getPriceByName("marker");
+		Stationary.getPriceByName("sketchPen");
+		Stationary.getPriceByName("crayons");
+		Stationary.getPriceByName("glue");
+		Stationary.getPriceByName("stapler");
+		Stationary.getPriceByName("punch");
+		Stationary.getPriceByName("file");
+		Stationary.getPriceByName("chart");
+		Stationary.getPriceByName("boardMarker");
+		Stationary.getPriceByName("tape");
+		Stationary.getPriceByName("highlighter");
+		Stationary.getPriceByName("compass");
+
+	}
+}
