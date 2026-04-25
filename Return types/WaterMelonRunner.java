3d132743acfb4aad1d0f2524a4f944ca1658bccb
prajwal shetter <198955243+PrajwalShetter@users.noreply.github.com>
2026-04-25 23:14:30 +0530

    Return type -- practice program

---
+++ Return types/WaterMelonRunner.java
@@ -0,0 +1,56 @@
+class WaterMelonRunner{
+
+	public static void main(String[] args){
+
+		double price = WaterMelon.getPriceByQuantity(1);
+		System.out.println(price);
+
+		price = WaterMelon.getPriceByQuantity(2);
+		System.out.println(price);
+
+		price = WaterMelon.getPriceByQuantity(3);
+		System.out.println(price);
+
+		price = WaterMelon.getPriceByQuantity(4);
+		System.out.println(price);
+
+		price = WaterMelon.getPriceByQuantity(5);
+		System.out.println(price);
+
+		price = WaterMelon.getPriceByQuantity(6);
+		System.out.println(price);
+
+		price = WaterMelon.getPriceByQuantity(7);
+		System.out.println(price);
+
+		price = WaterMelon.getPriceByQuantity(8);
+		System.out.println(price);
+
+		price = WaterMelon.getPriceByQuantity(9);
+		System.out.println(price);
+
+		price = WaterMelon.getPriceByQuantity(10);
+		System.out.println(price);
+
+		int qty = 1;
+		price = WaterMelon.getPriceByQuantity(qty);
+		System.out.println(price);
+
+		qty = 2;
+		price = WaterMelon.getPriceByQuantity(qty);
+		System.out.println(price);
+
+		qty = 3;
+		price = WaterMelon.getPriceByQuantity(qty);
+		System.out.println(price);
+
+		qty = 4;
+		price = WaterMelon.getPriceByQuantity(qty);
+		System.out.println(price);
+
+		qty = 5;
+		price = WaterMelon.getPriceByQuantity(qty);
+		System.out.println(price);
+
+	}
+}
