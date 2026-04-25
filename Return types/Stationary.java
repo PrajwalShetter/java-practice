7100f1d43cac29c721a3dea81c6dfbc0f29dee1d
prajwal shetter <198955243+PrajwalShetter@users.noreply.github.com>
2026-04-25 23:10:49 +0530

    Return type -- practice program

---
+++ Return types/Stationary.java
@@ -0,0 +1,78 @@
+class Stationary{
+
+	static void getPriceByName(String name){
+
+		System.out.println("The price of the Stationary :"+name);
+
+		if("pen" == name){
+			System.out.println("Price :"+10);
+			return;
+		}
+		if("pencil" == name){
+			System.out.println("Price :"+5);
+			return;
+		}
+		if("eraser" == name){
+			System.out.println("Price :"+3);
+			return;
+		}
+		if("sharpener" == name){
+			System.out.println("Price :"+7);
+			return;
+		}
+		if("notebook" == name){
+			System.out.println("Price :"+40);
+			return;
+		}
+		if("scale" == name){
+			System.out.println("Price :"+15);
+			return;
+		}
+		if("marker" == name){
+			System.out.println("Price :"+25);
+			return;
+		}
+		if("sketchPen" == name){
+			System.out.println("Price :"+60);
+			return;
+		}
+		if("crayons" == name){
+			System.out.println("Price :"+80);
+			return;
+		}
+		if("glue" == name){
+			System.out.println("Price :"+30);
+			return;
+		}
+		if("stapler" == name){
+			System.out.println("Price :"+120);
+			return;
+		}
+		if("punch" == name){
+			System.out.println("Price :"+150);
+			return;
+		}
+		if("file" == name){
+			System.out.println("Price :"+50);
+			return;
+		}
+		if("chart" == name){
+			System.out.println("Price :"+20);
+			return;
+		}
+		if("boardMarker" == name){
+			System.out.println("Price :"+35);
+			return;
+		}
+		if("tape" == name){
+			System.out.println("Price :"+25);
+			return;
+		}
+		if("highlighter" == name){
+			System.out.println("Price :"+45);
+			return;
+		}
+
+		System.out.println("No items are matched");
+	}
+}
