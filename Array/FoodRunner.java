cdeef97a626f5b4087a32ccbbb3193687bbe61be
prajwal shetter <198955243+PrajwalShetter@users.noreply.github.com>
2026-04-28 23:08:58 +0530

    Array practice programs

---
+++ Array/FoodRunner.java
@@ -0,0 +1,413 @@
+class FoodRunner{
+
+	public static void main(String[] values){
+		
+		String name="biriyani";
+		String[] item=FoodItem.getIngridients(name);
+		
+		
+		if(item!=null){
+			System.out.println("the item lenght is :"+item.length);
+			
+			for(String ref:item){
+				
+				System.out.println("the item required is:"+ref);
+			}
+			
+		}
+		name="poha";
+		item=FoodItem.getIngridients(name);
+		
+		
+		if(item!=null){
+			System.out.println("the item lenght is :"+item.length);
+			
+			for(String ref:item){
+				
+				System.out.println("the item required is:"+ref);
+			}
+			
+		}
+		name="dosa";
+		item=FoodItem.getIngridients(name);
+		
+		
+		if(item!=null){
+			System.out.println("the item lenght is :"+item.length);
+			
+			for(String ref:item){
+				
+				System.out.println("the item required is:"+ref);
+			}
+			
+		}
+		name="idli";
+		item=FoodItem.getIngridients(name);
+		
+		
+		if(item!=null){
+			System.out.println("the item lenght is :"+item.length);
+			
+			for(String ref:item){
+				
+				System.out.println("the item required is:"+ref);
+			}
+			
+		}
+		name="upma";
+		item=FoodItem.getIngridients(name);
+		
+		
+		if(item!=null){
+			System.out.println("the item lenght is :"+item.length);
+			
+			for(String ref:item){
+				
+				System.out.println("the item required is:"+ref);
+			}
+			
+		}
+		name="pongal";
+		item=FoodItem.getIngridients(name);
+		
+		
+		if(item!=null){
+			System.out.println("the item lenght is :"+item.length);
+			
+			for(String ref:item){
+				
+				System.out.println("the item required is:"+ref);
+			}
+			
+		}
+		name="vada";
+		item=FoodItem.getIngridients(name);
+		
+		
+		if(item!=null){
+			System.out.println("the item lenght is :"+item.length);
+			
+			for(String ref:item){
+				
+				System.out.println("the item required is:"+ref);
+			}
+			
+		}
+		name="puri";
+		item=FoodItem.getIngridients(name);
+		
+		
+		if(item!=null){
+			System.out.println("the item lenght is :"+item.length);
+			
+			for(String ref:item){
+				
+				System.out.println("the item required is:"+ref);
+			}
+			
+		}
+		name="chapati";
+		item=FoodItem.getIngridients(name);
+		
+		
+		if(item!=null){
+			System.out.println("the item lenght is :"+item.length);
+			
+			for(String ref:item){
+				
+				System.out.println("the item required is:"+ref);
+			}
+			
+		}
+		name="paratha";
+		item=FoodItem.getIngridients(name);
+		
+		
+		if(item!=null){
+			System.out.println("the item lenght is :"+item.length);
+			
+			for(String ref:item){
+				
+				System.out.println("the item required is:"+ref);
+			}
+			
+		}
+		name="fried rice";
+		item=FoodItem.getIngridients(name);
+		
+		
+		if(item!=null){
+			System.out.println("the item lenght is :"+item.length);
+			
+			for(String ref:item){
+				
+				System.out.println("the item required is:"+ref);
+			}
+			
+		}
+		name="noodles";
+		item=FoodItem.getIngridients(name);
+		
+		
+		if(item!=null){
+			System.out.println("the item lenght is :"+item.length);
+			
+			for(String ref:item){
+				
+				System.out.println("the item required is:"+ref);
+			}
+			
+		}
+		name="pizza";
+		item=FoodItem.getIngridients(name);
+		
+		
+		if(item!=null){
+			System.out.println("the item lenght is :"+item.length);
+			
+			for(String ref:item){
+				
+				System.out.println("the item required is:"+ref);
+			}
+			
+		}
+		name="burger";
+		item=FoodItem.getIngridients(name);
+		
+		
+		if(item!=null){
+			System.out.println("the item lenght is :"+item.length);
+			
+			for(String ref:item){
+				
+				System.out.println("the item required is:"+ref);
+			}
+			
+		}
+		name="sandwich";
+		item=FoodItem.getIngridients(name);
+		
+		
+		if(item!=null){
+			System.out.println("the item lenght is :"+item.length);
+			
+			for(String ref:item){
+				
+				System.out.println("the item required is:"+ref);
+			}
+			
+		}
+		name="pasta";
+		item=FoodItem.getIngridients(name);
+		
+		
+		if(item!=null){
+			System.out.println("the item lenght is :"+item.length);
+			
+			for(String ref:item){
+				
+				System.out.println("the item required is:"+ref);
+			}
+			
+		}
+		name="samosa";
+		item=FoodItem.getIngridients(name);
+		
+		
+		if(item!=null){
+			System.out.println("the item lenght is :"+item.length);
+			
+			for(String ref:item){
+				
+				System.out.println("the item required is:"+ref);
+			}
+			
+		}
+		name="pav bhaji";
+		item=FoodItem.getIngridients(name);
+		
+		
+		if(item!=null){
+			System.out.println("the item lenght is :"+item.length);
+			
+			for(String ref:item){
+				
+				System.out.println("the item required is:"+ref);
+			}
+			
+		}
+		name="chole bhature";
+		item=FoodItem.getIngridients(name);
+		
+		
+		if(item!=null){
+			System.out.println("the item lenght is :"+item.length);
+			
+			for(String ref:item){
+				
+				System.out.println("the item required is:"+ref);
+			}
+			
+		}
+		name="rajma rice";
+		item=FoodItem.getIngridients(name);
+		
+		
+		if(item!=null){
+			System.out.println("the item lenght is :"+item.length);
+			
+			for(String ref:item){
+				
+				System.out.println("the item required is:"+ref);
+			}
+			
+		}
+		name="dal tadka";
+		item=FoodItem.getIngridients(name);
+		
+		
+		if(item!=null){
+			System.out.println("the item lenght is :"+item.length);
+			
+			for(String ref:item){
+				
+				System.out.println("the item required is:"+ref);
+			}
+			
+		}
+		name="paneer butter masala";
+		item=FoodItem.getIngridients(name);
+		
+		
+		if(item!=null){
+			System.out.println("the item lenght is :"+item.length);
+			
+			for(String ref:item){
+				
+				System.out.println("the item required is:"+ref);
+			}
+			
+		}
+		name="chicken curry";
+		item=FoodItem.getIngridients(name);
+		
+		
+		if(item!=null){
+			System.out.println("the item lenght is :"+item.length);
+			
+			for(String ref:item){
+				
+				System.out.println("the item required is:"+ref);
+			}
+			
+		}
+		name="fish fry";
+		item=FoodItem.getIngridients(name);
+		
+		
+		if(item!=null){
+			System.out.println("the item lenght is :"+item.length);
+			
+			for(String ref:item){
+				
+				System.out.println("the item required is:"+ref);
+			}
+			
+		}
+		name="omelette";
+		item=FoodItem.getIngridients(name);
+		
+		
+		if(item!=null){
+			System.out.println("the item lenght is :"+item.length);
+			
+			for(String ref:item){
+				
+				System.out.println("the item required is:"+ref);
+			}
+			
+		}
+		name="maggi";
+		item=FoodItem.getIngridients(name);
+		
+		
+		if(item!=null){
+			System.out.println("the item lenght is :"+item.length);
+			
+			for(String ref:item){
+				
+				System.out.println("the item required is:"+ref);
+			}
+			
+		}
+		name="momos";
+		item=FoodItem.getIngridients(name);
+		
+		
+		if(item!=null){
+			System.out.println("the item lenght is :"+item.length);
+			
+			for(String ref:item){
+				
+				System.out.println("the item required is:"+ref);
+			}
+			
+		}
+		name="spring rolls";
+		item=FoodItem.getIngridients(name);
+		
+		
+		if(item!=null){
+			System.out.println("the item lenght is :"+item.length);
+			
+			for(String ref:item){
+				
+				System.out.println("the item required is:"+ref);
+			}
+			
+		}
+		name="ice cream";
+		item=FoodItem.getIngridients(name);
+		
+		
+		if(item!=null){
+			System.out.println("the item lenght is :"+item.length);
+			
+			for(String ref:item){
+				
+				System.out.println("the item required is:"+ref);
+			}
+			
+		}
+		name="gulab jamun";
+		item=FoodItem.getIngridients(name);
+		
+		
+		if(item!=null){
+			System.out.println("the item lenght is :"+item.length);
+			
+			for(String ref:item){
+				
+				System.out.println("the item required is:"+ref);
+			}
+			
+		}
+		name="jalebi";
+		item=FoodItem.getIngridients(name);
+		
+		
+		if(item!=null){
+			System.out.println("the item lenght is :"+item.length);
+			
+			for(String ref:item){
+				
+				System.out.println("the item required is:"+ref);
+			}
+			
+		}
+		
+		
+		
+	}
+	
+	}
