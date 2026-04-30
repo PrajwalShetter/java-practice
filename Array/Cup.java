3274f786b4882db128cab51993f54a074c04bfda
prajwal shetter <198955243+PrajwalShetter@users.noreply.github.com>
2026-04-30 22:55:23 +0530

    Array practice programs

---
+++ Array/Cup.java
@@ -0,0 +1,50 @@
+class Cup{
+	
+	String[] names = new String[5];
+	int index;
+	
+	void save(String name){
+		
+		System.out.println("Running save from cup");
+		System.out.println("name arg :"+name);
+		if(this.names != null){
+			System.out.println("The arrray is not null, saving in cup");
+			
+			if(this.index<this.names.length){
+				this.names[this.index]=name;
+				System.out.println("the name is store in index :"+this.index);
+				this.index++;
+			}	
+		}
+		else{
+			System.out.println("the array is null");
+		}
+		
+	}
+	
+	void search(String name){
+		
+		if(name != null){
+			
+			System.out.println("the name is not null, will search");
+			boolean found= false;
+			for(String temp : this.names){
+				if(name == temp){
+					
+					System.out.println("Cup is found");
+					found = true;
+					break;
+				}
+				
+			}
+			if(!found){
+				System.out.println("The cup is not found");
+			}
+		}
+		else{
+			
+			System.out.println("the name is null");
+		}
+	}
+	
+}
