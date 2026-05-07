94012ff390bf3b959791063d5e7db2c30395cd2c
prajwal shetter <198955243+PrajwalShetter@users.noreply.github.com>
2026-05-07 23:05:47 +0530

    constructor with istances

---
+++ Constructor and instances/ThreadMill.java
@@ -0,0 +1,29 @@
+class ThreadMill {
+
+    String brand;
+    String color;
+    int price;
+    double speed;
+    String type;
+    boolean automatic;
+    long id;
+    String material;
+    double weight;
+    boolean foldable;
+
+  
+    ThreadMill(String brand, String color, int price, double speed, String type,
+               boolean automatic, long id, String material, double weight, boolean foldable) {
+
+        this.brand = brand;
+        this.color = color;
+        this.price = price;
+        this.speed = speed;
+        this.type = type;
+        this.automatic = automatic;
+        this.id = id;
+        this.material = material;
+        this.weight = weight;
+        this.foldable = foldable;
+    }
+}
