046b81799db60fca01263946dc31b3206d57c96f
prajwal shetter <198955243+PrajwalShetter@users.noreply.github.com>
2026-05-08 22:17:27 +0530

    constructor with istances

---
+++ Constructor and instances/FishTank.java
@@ -0,0 +1,31 @@
+class FishTank {
+
+    String brand;
+    String color;
+    int capacity;
+    String shape;
+    boolean hasFilter;
+    double price;
+    String material;
+    double weight;
+    int fishCount;
+    boolean hasLight;
+
+    
+    FishTank(String brand, String color, int capacity, String shape, boolean hasFilter,
+             double price, String material, double weight, int fishCount, boolean hasLight) {
+
+        this.brand = brand;
+        this.color = color;
+        this.capacity = capacity;
+        this.shape = shape;
+        this.hasFilter = hasFilter;
+        this.price = price;
+        this.material = material;
+        this.weight = weight;
+        this.fishCount = fishCount;
+        this.hasLight = hasLight;
+    }
+
+}
+
