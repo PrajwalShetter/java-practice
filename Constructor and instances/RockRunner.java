87579e7bf070cc929f627654e2025f6aa19f764f
prajwal shetter <198955243+PrajwalShetter@users.noreply.github.com>
2026-05-08 22:14:10 +0530

    constructor with istances

---
+++ Constructor and instances/RockRunner.java
@@ -0,0 +1,382 @@
+class RockRunner {
+
+    public static void main(String... args) {
+
+        Rock rock1 = new Rock("Granite", "Gray", 50, "India", true,
+                701L, "Mineral", 2.7, 5.5, false);
+
+        System.out.println("This is the first instance :");
+        System.out.println("Type : " + rock1.type);
+        System.out.println("Color : " + rock1.color);
+        System.out.println("Weight : " + rock1.weight);
+        System.out.println("Origin : " + rock1.origin);
+        System.out.println("Hard : " + rock1.hard);
+        System.out.println("ID : " + rock1.id);
+        System.out.println("Material : " + rock1.material);
+        System.out.println("Density : " + rock1.density);
+        System.out.println("Size : " + rock1.size);
+        System.out.println("Valuable : " + rock1.valuable);
+
+        System.out.println("\n");
+
+        Rock rock2 = new Rock("Basalt", "Black", 60, "USA", true, 702L, "Mineral", 3.0, 6.0, false);
+        System.out.println("This is the second instance :");
+        System.out.println("Type : " + rock2.type);
+        System.out.println("Color : " + rock2.color);
+        System.out.println("Weight : " + rock2.weight);
+        System.out.println("Origin : " + rock2.origin);
+        System.out.println("Hard : " + rock2.hard);
+        System.out.println("ID : " + rock2.id);
+        System.out.println("Material : " + rock2.material);
+        System.out.println("Density : " + rock2.density);
+        System.out.println("Size : " + rock2.size);
+        System.out.println("Valuable : " + rock2.valuable);
+
+        System.out.println("\n");
+
+        Rock rock3 = new Rock("Marble", "White", 40, "Italy", false, 703L, "Mineral", 2.6, 5.0, true);
+        System.out.println("This is the third instance :");
+        System.out.println("Type : " + rock3.type);
+        System.out.println("Color : " + rock3.color);
+        System.out.println("Weight : " + rock3.weight);
+        System.out.println("Origin : " + rock3.origin);
+        System.out.println("Hard : " + rock3.hard);
+        System.out.println("ID : " + rock3.id);
+        System.out.println("Material : " + rock3.material);
+        System.out.println("Density : " + rock3.density);
+        System.out.println("Size : " + rock3.size);
+        System.out.println("Valuable : " + rock3.valuable);
+
+        System.out.println("\n");
+
+        Rock rock4 = new Rock("Limestone", "Gray", 45, "UK", false, 704L, "Mineral", 2.3, 4.8, false);
+        System.out.println("This is the fourth instance :");
+        System.out.println("Type : " + rock4.type);
+        System.out.println("Color : " + rock4.color);
+        System.out.println("Weight : " + rock4.weight);
+        System.out.println("Origin : " + rock4.origin);
+        System.out.println("Hard : " + rock4.hard);
+        System.out.println("ID : " + rock4.id);
+        System.out.println("Material : " + rock4.material);
+        System.out.println("Density : " + rock4.density);
+        System.out.println("Size : " + rock4.size);
+        System.out.println("Valuable : " + rock4.valuable);
+
+        System.out.println("\n");
+
+        Rock rock5 = new Rock("Sandstone", "Brown", 35, "India", false, 705L, "Mineral", 2.2, 4.5, false);
+        System.out.println("This is the fifth instance :");
+        System.out.println("Type : " + rock5.type);
+        System.out.println("Color : " + rock5.color);
+        System.out.println("Weight : " + rock5.weight);
+        System.out.println("Origin : " + rock5.origin);
+        System.out.println("Hard : " + rock5.hard);
+        System.out.println("ID : " + rock5.id);
+        System.out.println("Material : " + rock5.material);
+        System.out.println("Density : " + rock5.density);
+        System.out.println("Size : " + rock5.size);
+        System.out.println("Valuable : " + rock5.valuable);
+
+        System.out.println("\n");
+
+        Rock rock6 = new Rock("Quartz", "Clear", 30, "Brazil", true, 706L, "Crystal", 2.7, 4.0, true);
+        System.out.println("This is the sixth instance :");
+        System.out.println("Type : " + rock6.type);
+        System.out.println("Color : " + rock6.color);
+        System.out.println("Weight : " + rock6.weight);
+        System.out.println("Origin : " + rock6.origin);
+        System.out.println("Hard : " + rock6.hard);
+        System.out.println("ID : " + rock6.id);
+        System.out.println("Material : " + rock6.material);
+        System.out.println("Density : " + rock6.density);
+        System.out.println("Size : " + rock6.size);
+        System.out.println("Valuable : " + rock6.valuable);
+
+        System.out.println("\n");
+
+        Rock rock7 = new Rock("Obsidian", "Black", 25, "Mexico", true, 707L, "Glass", 2.4, 3.5, true);
+        System.out.println("This is the seventh instance :");
+        System.out.println("Type : " + rock7.type);
+        System.out.println("Color : " + rock7.color);
+        System.out.println("Weight : " + rock7.weight);
+        System.out.println("Origin : " + rock7.origin);
+        System.out.println("Hard : " + rock7.hard);
+        System.out.println("ID : " + rock7.id);
+        System.out.println("Material : " + rock7.material);
+        System.out.println("Density : " + rock7.density);
+        System.out.println("Size : " + rock7.size);
+        System.out.println("Valuable : " + rock7.valuable);
+
+        System.out.println("\n");
+
+        Rock rock8 = new Rock("Slate", "Gray", 50, "Spain", true, 708L, "Mineral", 2.8, 5.2, false);
+        System.out.println("This is the eighth instance :");
+        System.out.println("Type : " + rock8.type);
+        System.out.println("Color : " + rock8.color);
+        System.out.println("Weight : " + rock8.weight);
+        System.out.println("Origin : " + rock8.origin);
+        System.out.println("Hard : " + rock8.hard);
+        System.out.println("ID : " + rock8.id);
+        System.out.println("Material : " + rock8.material);
+        System.out.println("Density : " + rock8.density);
+        System.out.println("Size : " + rock8.size);
+        System.out.println("Valuable : " + rock8.valuable);
+
+        System.out.println("\n");
+
+        Rock rock9 = new Rock("Gneiss", "Striped", 55, "Canada", true, 709L, "Mineral", 2.9, 5.8, false);
+        System.out.println("This is the ninth instance :");
+        System.out.println("Type : " + rock9.type);
+        System.out.println("Color : " + rock9.color);
+        System.out.println("Weight : " + rock9.weight);
+        System.out.println("Origin : " + rock9.origin);
+        System.out.println("Hard : " + rock9.hard);
+        System.out.println("ID : " + rock9.id);
+        System.out.println("Material : " + rock9.material);
+        System.out.println("Density : " + rock9.density);
+        System.out.println("Size : " + rock9.size);
+        System.out.println("Valuable : " + rock9.valuable);
+
+        System.out.println("\n");
+
+        Rock rock10 = new Rock("Shale", "Dark Gray", 42, "China", false, 710L, "Mineral", 2.5, 4.7, false);
+        System.out.println("This is the tenth instance :");
+        System.out.println("Type : " + rock10.type);
+        System.out.println("Color : " + rock10.color);
+        System.out.println("Weight : " + rock10.weight);
+        System.out.println("Origin : " + rock10.origin);
+        System.out.println("Hard : " + rock10.hard);
+        System.out.println("ID : " + rock10.id);
+        System.out.println("Material : " + rock10.material);
+        System.out.println("Density : " + rock10.density);
+        System.out.println("Size : " + rock10.size);
+        System.out.println("Valuable : " + rock10.valuable);
+
+        System.out.println("\n");
+
+        Rock rock11 = new Rock("Pumice", "Light Gray", 20, "Japan", false, 711L, "Volcanic", 1.0, 3.0, false);
+        System.out.println("This is the eleventh instance :");
+        System.out.println("Type : " + rock11.type);
+        System.out.println("Color : " + rock11.color);
+        System.out.println("Weight : " + rock11.weight);
+        System.out.println("Origin : " + rock11.origin);
+        System.out.println("Hard : " + rock11.hard);
+        System.out.println("ID : " + rock11.id);
+        System.out.println("Material : " + rock11.material);
+        System.out.println("Density : " + rock11.density);
+        System.out.println("Size : " + rock11.size);
+        System.out.println("Valuable : " + rock11.valuable);
+
+        System.out.println("\n");
+
+        Rock rock12 = new Rock("Granite", "Pink", 60, "India", true, 712L, "Mineral", 2.7, 6.0, false);
+        System.out.println("This is the twelfth instance :");
+        System.out.println("Type : " + rock12.type);
+        System.out.println("Color : " + rock12.color);
+        System.out.println("Weight : " + rock12.weight);
+        System.out.println("Origin : " + rock12.origin);
+        System.out.println("Hard : " + rock12.hard);
+        System.out.println("ID : " + rock12.id);
+        System.out.println("Material : " + rock12.material);
+        System.out.println("Density : " + rock12.density);
+        System.out.println("Size : " + rock12.size);
+        System.out.println("Valuable : " + rock12.valuable);
+
+        System.out.println("\n");
+
+        Rock rock13 = new Rock("Basalt", "Dark Black", 65, "USA", true, 713L, "Mineral", 3.1, 6.5, false);
+        System.out.println("This is the thirteenth instance :");
+        System.out.println("Type : " + rock13.type);
+        System.out.println("Color : " + rock13.color);
+        System.out.println("Weight : " + rock13.weight);
+        System.out.println("Origin : " + rock13.origin);
+        System.out.println("Hard : " + rock13.hard);
+        System.out.println("ID : " + rock13.id);
+        System.out.println("Material : " + rock13.material);
+        System.out.println("Density : " + rock13.density);
+        System.out.println("Size : " + rock13.size);
+        System.out.println("Valuable : " + rock13.valuable);
+
+        System.out.println("\n");
+
+        Rock rock14 = new Rock("Marble", "Green", 38, "Italy", false, 714L, "Mineral", 2.6, 4.9, true);
+        System.out.println("This is the fourteenth instance :");
+        System.out.println("Type : " + rock14.type);
+        System.out.println("Color : " + rock14.color);
+        System.out.println("Weight : " + rock14.weight);
+        System.out.println("Origin : " + rock14.origin);
+        System.out.println("Hard : " + rock14.hard);
+        System.out.println("ID : " + rock14.id);
+        System.out.println("Material : " + rock14.material);
+        System.out.println("Density : " + rock14.density);
+        System.out.println("Size : " + rock14.size);
+        System.out.println("Valuable : " + rock14.valuable);
+
+        System.out.println("\n");
+
+        Rock rock15 = new Rock("Limestone", "Beige", 48, "UK", false, 715L, "Mineral", 2.3, 5.1, false);
+        System.out.println("This is the fifteenth instance :");
+        System.out.println("Type : " + rock15.type);
+        System.out.println("Color : " + rock15.color);
+        System.out.println("Weight : " + rock15.weight);
+        System.out.println("Origin : " + rock15.origin);
+        System.out.println("Hard : " + rock15.hard);
+        System.out.println("ID : " + rock15.id);
+        System.out.println("Material : " + rock15.material);
+        System.out.println("Density : " + rock15.density);
+        System.out.println("Size : " + rock15.size);
+        System.out.println("Valuable : " + rock15.valuable);
+
+        System.out.println("\n");
+
+        Rock rock16 = new Rock("Sandstone", "Red", 36, "India", false, 716L, "Mineral", 2.2, 4.6, false);
+        System.out.println("This is the sixteenth instance :");
+        System.out.println("Type : " + rock16.type);
+        System.out.println("Color : " + rock16.color);
+        System.out.println("Weight : " + rock16.weight);
+        System.out.println("Origin : " + rock16.origin);
+        System.out.println("Hard : " + rock16.hard);
+        System.out.println("ID : " + rock16.id);
+        System.out.println("Material : " + rock16.material);
+        System.out.println("Density : " + rock16.density);
+        System.out.println("Size : " + rock16.size);
+        System.out.println("Valuable : " + rock16.valuable);
+
+        System.out.println("\n");
+
+        Rock rock17 = new Rock("Quartz", "White", 32, "Brazil", true, 717L, "Crystal", 2.7, 4.2, true);
+        System.out.println("This is the seventeenth instance :");
+        System.out.println("Type : " + rock17.type);
+        System.out.println("Color : " + rock17.color);
+        System.out.println("Weight : " + rock17.weight);
+        System.out.println("Origin : " + rock17.origin);
+        System.out.println("Hard : " + rock17.hard);
+        System.out.println("ID : " + rock17.id);
+        System.out.println("Material : " + rock17.material);
+        System.out.println("Density : " + rock17.density);
+        System.out.println("Size : " + rock17.size);
+        System.out.println("Valuable : " + rock17.valuable);
+
+        System.out.println("\n");
+
+        Rock rock18 = new Rock("Obsidian", "Glossy Black", 28, "Mexico", true, 718L, "Glass", 2.5, 3.8, true);
+        System.out.println("This is the eighteenth instance :");
+        System.out.println("Type : " + rock18.type);
+        System.out.println("Color : " + rock18.color);
+        System.out.println("Weight : " + rock18.weight);
+        System.out.println("Origin : " + rock18.origin);
+        System.out.println("Hard : " + rock18.hard);
+        System.out.println("ID : " + rock18.id);
+        System.out.println("Material : " + rock18.material);
+        System.out.println("Density : " + rock18.density);
+        System.out.println("Size : " + rock18.size);
+        System.out.println("Valuable : " + rock18.valuable);
+
+        System.out.println("\n");
+
+        Rock rock19 = new Rock("Slate", "Dark Gray", 52, "Spain", true, 719L, "Mineral", 2.8, 5.4, false);
+        System.out.println("This is the nineteenth instance :");
+        System.out.println("Type : " + rock19.type);
+        System.out.println("Color : " + rock19.color);
+        System.out.println("Weight : " + rock19.weight);
+        System.out.println("Origin : " + rock19.origin);
+        System.out.println("Hard : " + rock19.hard);
+        System.out.println("ID : " + rock19.id);
+        System.out.println("Material : " + rock19.material);
+        System.out.println("Density : " + rock19.density);
+        System.out.println("Size : " + rock19.size);
+        System.out.println("Valuable : " + rock19.valuable);
+
+        System.out.println("\n");
+
+        Rock rock20 = new Rock("Gneiss", "Gray White", 58, "Canada", true, 720L, "Mineral", 2.9, 6.0, false);
+        System.out.println("This is the twentieth instance :");
+        System.out.println("Type : " + rock20.type);
+        System.out.println("Color : " + rock20.color);
+        System.out.println("Weight : " + rock20.weight);
+        System.out.println("Origin : " + rock20.origin);
+        System.out.println("Hard : " + rock20.hard);
+        System.out.println("ID : " + rock20.id);
+        System.out.println("Material : " + rock20.material);
+        System.out.println("Density : " + rock20.density);
+        System.out.println("Size : " + rock20.size);
+        System.out.println("Valuable : " + rock20.valuable);
+
+        System.out.println("\n");
+
+        Rock rock21 = new Rock("Shale", "Black", 44, "China", false, 721L, "Mineral", 2.5, 4.9, false);
+        System.out.println("This is the twenty first instance :");
+        System.out.println("Type : " + rock21.type);
+        System.out.println("Color : " + rock21.color);
+        System.out.println("Weight : " + rock21.weight);
+        System.out.println("Origin : " + rock21.origin);
+        System.out.println("Hard : " + rock21.hard);
+        System.out.println("ID : " + rock21.id);
+        System.out.println("Material : " + rock21.material);
+        System.out.println("Density : " + rock21.density);
+        System.out.println("Size : " + rock21.size);
+        System.out.println("Valuable : " + rock21.valuable);
+
+        System.out.println("\n");
+
+        Rock rock22 = new Rock("Pumice", "White", 22, "Japan", false, 722L, "Volcanic", 1.1, 3.2, false);
+        System.out.println("This is the twenty second instance :");
+        System.out.println("Type : " + rock22.type);
+        System.out.println("Color : " + rock22.color);
+        System.out.println("Weight : " + rock22.weight);
+        System.out.println("Origin : " + rock22.origin);
+        System.out.println("Hard : " + rock22.hard);
+        System.out.println("ID : " + rock22.id);
+        System.out.println("Material : " + rock22.material);
+        System.out.println("Density : " + rock22.density);
+        System.out.println("Size : " + rock22.size);
+        System.out.println("Valuable : " + rock22.valuable);
+
+        System.out.println("\n");
+
+        Rock rock23 = new Rock("Granite", "Blue", 62, "India", true, 723L, "Mineral", 2.7, 6.2, false);
+        System.out.println("This is the twenty third instance :");
+        System.out.println("Type : " + rock23.type);
+        System.out.println("Color : " + rock23.color);
+        System.out.println("Weight : " + rock23.weight);
+        System.out.println("Origin : " + rock23.origin);
+        System.out.println("Hard : " + rock23.hard);
+        System.out.println("ID : " + rock23.id);
+        System.out.println("Material : " + rock23.material);
+        System.out.println("Density : " + rock23.density);
+        System.out.println("Size : " + rock23.size);
+        System.out.println("Valuable : " + rock23.valuable);
+
+        System.out.println("\n");
+
+        Rock rock24 = new Rock("Basalt", "Gray Black", 68, "USA", true, 724L, "Mineral", 3.2, 6.8, false);
+        System.out.println("This is the twenty fourth instance :");
+        System.out.println("Type : " + rock24.type);
+        System.out.println("Color : " + rock24.color);
+        System.out.println("Weight : " + rock24.weight);
+        System.out.println("Origin : " + rock24.origin);
+        System.out.println("Hard : " + rock24.hard);
+        System.out.println("ID : " + rock24.id);
+        System.out.println("Material : " + rock24.material);
+        System.out.println("Density : " + rock24.density);
+        System.out.println("Size : " + rock24.size);
+        System.out.println("Valuable : " + rock24.valuable);
+
+        System.out.println("\n");
+
+        Rock rock25 = new Rock("Marble", "Golden", 45, "Italy", false, 725L, "Mineral", 2.6, 5.3, true);
+        System.out.println("This is the twenty fifth instance :");
+        System.out.println("Type : " + rock25.type);
+        System.out.println("Color : " + rock25.color);
+        System.out.println("Weight : " + rock25.weight);
+        System.out.println("Origin : " + rock25.origin);
+        System.out.println("Hard : " + rock25.hard);
+        System.out.println("ID : " + rock25.id);
+        System.out.println("Material : " + rock25.material);
+        System.out.println("Density : " + rock25.density);
+        System.out.println("Size : " + rock25.size);
+        System.out.println("Valuable : " + rock25.valuable);
+
+        System.out.println("\n");
+    }
+}
