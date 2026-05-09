ac6065d0cfc7a03d263c6e9c1dea2f3773e12a95
prajwal shetter <198955243+PrajwalShetter@users.noreply.github.com>
2026-05-09 22:50:17 +0530

    constructor with istances

---
+++ Constructor and instances/DumbBells.java
@@ -0,0 +1,29 @@
+class DumbBells {
+
+    String brand;
+    String color;
+    int price;
+    double weight;
+    String type;
+    boolean adjustable;
+    long id;
+    String material;
+    int quantity;
+    boolean rubberCoated;
+
+    
+    DumbBells(String brand, String color, int price, double weight, String type,
+              boolean adjustable, long id, String material, int quantity, boolean rubberCoated) {
+
+        this.brand = brand;
+        this.color = color;
+        this.price = price;
+        this.weight = weight;
+        this.type = type;
+        this.adjustable = adjustable;
+        this.id = id;
+        this.material = material;
+        this.quantity = quantity;
+        this.rubberCoated = rubberCoated;
+    }
+}
