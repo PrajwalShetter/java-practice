c2d0b214a4517e21676966ff516c1130fa9cb607
prajwal shetter <198955243+PrajwalShetter@users.noreply.github.com>
2026-05-09 22:48:41 +0530

    constructor with istances

---
+++ Constructor and instances/BarBell.java
@@ -0,0 +1,29 @@
+class BarBell {
+
+    String brand;
+    String color;
+    int price;
+    double weight;
+    String type;
+    boolean adjustable;
+    long id;
+    String material;
+    double length;
+    boolean rubberGrip;
+
+    
+    BarBell(String brand, String color, int price, double weight, String type,
+            boolean adjustable, long id, String material, double length, boolean rubberGrip) {
+
+        this.brand = brand;
+        this.color = color;
+        this.price = price;
+        this.weight = weight;
+        this.type = type;
+        this.adjustable = adjustable;
+        this.id = id;
+        this.material = material;
+        this.length = length;
+        this.rubberGrip = rubberGrip;
+    }
+}
