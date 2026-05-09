c495a32b2c2b205c3d4004324553918a97c7735a
prajwal shetter <198955243+PrajwalShetter@users.noreply.github.com>
2026-05-09 22:47:40 +0530

    constructor with istances

---
+++ Constructor and instances/AsianPaint.java
@@ -0,0 +1,30 @@
+class AsianPaint {
+
+    String brand;
+    String color;
+    int price;
+    String type;
+    boolean waterproof;
+    long id;
+    String finish;
+    double quantity;
+    String usage;
+    boolean ecoFriendly;
+
+    
+    AsianPaint(String brand, String color, int price, String type,
+               boolean waterproof, long id, String finish,
+               double quantity, String usage, boolean ecoFriendly) {
+
+        this.brand = brand;
+        this.color = color;
+        this.price = price;
+        this.type = type;
+        this.waterproof = waterproof;
+        this.id = id;
+        this.finish = finish;
+        this.quantity = quantity;
+        this.usage = usage;
+        this.ecoFriendly = ecoFriendly;
+    }
+}
