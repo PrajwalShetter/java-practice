9452c3ca421ed4f1704284477f5b4e46c5fde571
prajwal shetter <198955243+PrajwalShetter@users.noreply.github.com>
2026-05-18 22:53:29 +0530

    Understanding collection framework -- CollectionClass

--- Collections/LearnCollectionFramework/src/LearnCollectionClass.java
+++ Collections/LearnCollectionFramework/src/LearnCollectionClass.java
@@ -28,5 +28,31 @@ public class LearnCollectionClass {
         Collections.sort(list, Comparator.reverseOrder());// it sorts the array in the reverse order
         System.out.println("Decnding order :"+list);
 
+        List<Student> list1 = new ArrayList<>();
+
+        list1.add(new Student("arjuna", 2));
+        list1.add(new Student("manikya", 1));
+        list1.add(new Student("prajwal", 4));
+        list1.add(new Student("praveen", 3));
+
+        Student s1 = new Student("ishan", 5);
+        Student s2 = new Student("zyan", 6);
+
+        System.out.println((s1.compareTo(s2)));
+
+        System.out.println(list1);
+        Collections.sort(list1);
+        System.out.println(list1);
+
+        Collections.sort(list1, new Comparator<Student>() {
+            @Override
+            public int compare(Student o1, Student o2) {
+                return o1.name.compareTo(o2.name);
+            }
+        });
+
+        System.out.println(list1);
+
+
     }
 }
