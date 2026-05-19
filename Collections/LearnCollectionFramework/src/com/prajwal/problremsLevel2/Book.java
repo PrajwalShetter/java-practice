427b436325b3c73d8bed829856c45f0ec51c2223
prajwal shetter <198955243+PrajwalShetter@users.noreply.github.com>
2026-05-19 23:11:24 +0530

    Collection based 2nd level problems

---
+++ Collections/LearnCollectionFramework/src/com/prajwal/problremsLevel2/Book.java
@@ -0,0 +1,13 @@
+package com.prajwal.problremsLevel2;
+
+public class Book {
+
+    String name;
+    int price;
+
+    public Book(String name, int price){
+        this.name = name;
+        this.price= price;
+
+    }
+}
