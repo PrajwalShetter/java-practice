d3882ba35acb6d70d99e65d7422b9c9952e003ce
prajwal shetter <198955243+PrajwalShetter@users.noreply.github.com>
2026-05-19 23:12:18 +0530

    Collection based 2nd level problems

---
+++ Collections/LearnCollectionFramework/src/com/prajwal/problremsLevel2/Employee.java
@@ -0,0 +1,18 @@
+package com.prajwal.problremsLevel2;
+
+public class Employee {
+
+    int id;
+    String name;
+    int salary;
+
+    public Employee(int id, String name, int salary){
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+
+    }
+
+
+
+}
