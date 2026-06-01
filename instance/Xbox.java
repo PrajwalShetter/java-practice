3c5988d2ca9e7c30a2e9f55e9181d750bca0302e
prajwal shetter <198955243+PrajwalShetter@users.noreply.github.com>
2026-06-01 23:44:30 +0530

    Learning instance

---
+++ instance/Xbox.java
@@ -0,0 +1,20 @@
+class Xbox{
+	
+	static String brand = "Microsoft";
+	static String color = "Black";
+	static double price = 45000;
+	static boolean wireless = true;
+	static int storage = 512;
+	
+	String model;
+	String processor;
+	double clockSpeed;
+	boolean support4k;
+	String controllerType;
+	int ports;
+	double powerConsumption;
+	String origin;
+	boolean onlineSupport;
+	String edition;
+}
+
