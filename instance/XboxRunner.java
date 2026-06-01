e87c1e496b464cb99debf90647ec5f7d1746fd24
prajwal shetter <198955243+PrajwalShetter@users.noreply.github.com>
2026-06-01 23:44:52 +0530

    Learning instance

---
+++ instance/XboxRunner.java
@@ -0,0 +1,272 @@
+class XboxRunner {
+
+	public static void main(String[] args) {
+
+		System.out.println("Brand :" + Xbox.brand);
+		System.out.println("Color :" + Xbox.color);
+		System.out.println("Price :" + Xbox.price);
+		System.out.println("Wireless :" + Xbox.wireless);
+		System.out.println("Storage :" + Xbox.storage);
+		System.out.println("\n");
+
+		Xbox x1 = new Xbox();
+		System.out.println("This is First Xbox \n");
+
+		x1.model = "Series X";
+		x1.processor = "AMD Zen 2";
+		x1.clockSpeed = 3.8;
+		x1.support4K = true;
+		x1.controllerType = "Wireless";
+		x1.ports = 3;
+		x1.powerConsumption = 315;
+		x1.origin = "USA";
+		x1.onlineSupport = true;
+		x1.edition = "Standard";
+
+		System.out.println("Model :" + x1.model);
+		System.out.println("Processor :" + x1.processor);
+		System.out.println("Clock :" + x1.clockSpeed);
+		System.out.println("4K :" + x1.support4K);
+		System.out.println("Controller :" + x1.controllerType);
+		System.out.println("Ports :" + x1.ports);
+		System.out.println("Power :" + x1.powerConsumption);
+		System.out.println("Origin :" + x1.origin);
+		System.out.println("Online :" + x1.onlineSupport);
+		System.out.println("Edition :" + x1.edition);
+		System.out.println("\n");
+
+		Xbox x2 = new Xbox();
+		System.out.println("This is Second Xbox \n");
+
+		x2.model = "Series S";
+		x2.processor = "AMD Zen 2";
+		x2.clockSpeed = 3.6;
+		x2.support4K = false;
+		x2.controllerType = "Wireless";
+		x2.ports = 2;
+		x2.powerConsumption = 165;
+		x2.origin = "China";
+		x2.onlineSupport = true;
+		x2.edition = "Digital";
+
+		System.out.println("Model :" + x2.model);
+		System.out.println("Processor :" + x2.processor);
+		System.out.println("Clock :" + x2.clockSpeed);
+		System.out.println("4K :" + x2.support4K);
+		System.out.println("Controller :" + x2.controllerType);
+		System.out.println("Ports :" + x2.ports);
+		System.out.println("Power :" + x2.powerConsumption);
+		System.out.println("Origin :" + x2.origin);
+		System.out.println("Online :" + x2.onlineSupport);
+		System.out.println("Edition :" + x2.edition);
+		System.out.println("\n");
+
+		Xbox x3 = new Xbox();
+		System.out.println("This is Third Xbox \n");
+
+		x3.model = "One X";
+		x3.processor = "Jaguar";
+		x3.clockSpeed = 2.3;
+		x3.support4K = true;
+		x3.controllerType = "Wireless";
+		x3.ports = 3;
+		x3.powerConsumption = 245;
+		x3.origin = "USA";
+		x3.onlineSupport = true;
+		x3.edition = "Pro";
+
+		System.out.println("Model :" + x3.model);
+		System.out.println("Processor :" + x3.processor);
+		System.out.println("Clock :" + x3.clockSpeed);
+		System.out.println("4K :" + x3.support4K);
+		System.out.println("Controller :" + x3.controllerType);
+		System.out.println("Ports :" + x3.ports);
+		System.out.println("Power :" + x3.powerConsumption);
+		System.out.println("Origin :" + x3.origin);
+		System.out.println("Online :" + x3.onlineSupport);
+		System.out.println("Edition :" + x3.edition);
+		System.out.println("\n");
+
+		Xbox x4 = new Xbox();
+		System.out.println("This is Fourth Xbox \n");
+
+		x4.model = "One S";
+		x4.processor = "Jaguar";
+		x4.clockSpeed = 1.75;
+		x4.support4K = false;
+		x4.controllerType = "Wireless";
+		x4.ports = 2;
+		x4.powerConsumption = 120;
+		x4.origin = "China";
+		x4.onlineSupport = true;
+		x4.edition = "Standard";
+
+		System.out.println("Model :" + x4.model);
+		System.out.println("Processor :" + x4.processor);
+		System.out.println("Clock :" + x4.clockSpeed);
+		System.out.println("4K :" + x4.support4K);
+		System.out.println("Controller :" + x4.controllerType);
+		System.out.println("Ports :" + x4.ports);
+		System.out.println("Power :" + x4.powerConsumption);
+		System.out.println("Origin :" + x4.origin);
+		System.out.println("Online :" + x4.onlineSupport);
+		System.out.println("Edition :" + x4.edition);
+		System.out.println("\n");
+
+		Xbox x5 = new Xbox();
+		System.out.println("This is Fifth Xbox \n");
+
+		x5.model = "Series X Halo Edition";
+		x5.processor = "AMD Zen 2";
+		x5.clockSpeed = 3.8;
+		x5.support4K = true;
+		x5.controllerType = "Wireless";
+		x5.ports = 3;
+		x5.powerConsumption = 315;
+		x5.origin = "USA";
+		x5.onlineSupport = true;
+		x5.edition = "Limited";
+
+		System.out.println("Model :" + x5.model);
+		System.out.println("Processor :" + x5.processor);
+		System.out.println("Clock :" + x5.clockSpeed);
+		System.out.println("4K :" + x5.support4K);
+		System.out.println("Controller :" + x5.controllerType);
+		System.out.println("Ports :" + x5.ports);
+		System.out.println("Power :" + x5.powerConsumption);
+		System.out.println("Origin :" + x5.origin);
+		System.out.println("Online :" + x5.onlineSupport);
+		System.out.println("Edition :" + x5.edition);
+		System.out.println("\n");
+
+		Xbox x6 = new Xbox();
+		System.out.println("This is Sixth Xbox \n");
+
+		x6.model = "Series S Carbon";
+		x6.processor = "AMD Zen 2";
+		x6.clockSpeed = 3.6;
+		x6.support4K = false;
+		x6.controllerType = "Wireless";
+		x6.ports = 2;
+		x6.powerConsumption = 165;
+		x6.origin = "China";
+		x6.onlineSupport = true;
+		x6.edition = "Special";
+
+		System.out.println("Model :" + x6.model);
+		System.out.println("Processor :" + x6.processor);
+		System.out.println("Clock :" + x6.clockSpeed);
+		System.out.println("4K :" + x6.support4K);
+		System.out.println("Controller :" + x6.controllerType);
+		System.out.println("Ports :" + x6.ports);
+		System.out.println("Power :" + x6.powerConsumption);
+		System.out.println("Origin :" + x6.origin);
+		System.out.println("Online :" + x6.onlineSupport);
+		System.out.println("Edition :" + x6.edition);
+		System.out.println("\n");
+
+		Xbox x7 = new Xbox();
+		System.out.println("This is Seventh Xbox \n");
+
+		x7.model = "One Elite";
+		x7.processor = "Jaguar";
+		x7.clockSpeed = 2.0;
+		x7.support4K = false;
+		x7.controllerType = "Elite";
+		x7.ports = 3;
+		x7.powerConsumption = 200;
+		x7.origin = "USA";
+		x7.onlineSupport = true;
+		x7.edition = "Elite";
+
+		System.out.println("Model :" + x7.model);
+		System.out.println("Processor :" + x7.processor);
+		System.out.println("Clock :" + x7.clockSpeed);
+		System.out.println("4K :" + x7.support4K);
+		System.out.println("Controller :" + x7.controllerType);
+		System.out.println("Ports :" + x7.ports);
+		System.out.println("Power :" + x7.powerConsumption);
+		System.out.println("Origin :" + x7.origin);
+		System.out.println("Online :" + x7.onlineSupport);
+		System.out.println("Edition :" + x7.edition);
+		System.out.println("\n");
+
+		Xbox x8 = new Xbox();
+		System.out.println("This is Eighth Xbox \n");
+
+		x8.model = "One Digital";
+		x8.processor = "Jaguar";
+		x8.clockSpeed = 1.75;
+		x8.support4K = false;
+		x8.controllerType = "Wireless";
+		x8.ports = 2;
+		x8.powerConsumption = 110;
+		x8.origin = "China";
+		x8.onlineSupport = true;
+		x8.edition = "Digital";
+
+		System.out.println("Model :" + x8.model);
+		System.out.println("Processor :" + x8.processor);
+		System.out.println("Clock :" + x8.clockSpeed);
+		System.out.println("4K :" + x8.support4K);
+		System.out.println("Controller :" + x8.controllerType);
+		System.out.println("Ports :" + x8.ports);
+		System.out.println("Power :" + x8.powerConsumption);
+		System.out.println("Origin :" + x8.origin);
+		System.out.println("Online :" + x8.onlineSupport);
+		System.out.println("Edition :" + x8.edition);
+		System.out.println("\n");
+
+		Xbox x9 = new Xbox();
+		System.out.println("This is Ninth Xbox \n");
+
+		x9.model = "Series X Mini";
+		x9.processor = "AMD Zen 2";
+		x9.clockSpeed = 3.5;
+		x9.support4K = true;
+		x9.controllerType = "Wireless";
+		x9.ports = 2;
+		x9.powerConsumption = 280;
+		x9.origin = "USA";
+		x9.onlineSupport = true;
+		x9.edition = "Compact";
+
+		System.out.println("Model :" + x9.model);
+		System.out.println("Processor :" + x9.processor);
+		System.out.println("Clock :" + x9.clockSpeed);
+		System.out.println("4K :" + x9.support4K);
+		System.out.println("Controller :" + x9.controllerType);
+		System.out.println("Ports :" + x9.ports);
+		System.out.println("Power :" + x9.powerConsumption);
+		System.out.println("Origin :" + x9.origin);
+		System.out.println("Online :" + x9.onlineSupport);
+		System.out.println("Edition :" + x9.edition);
+		System.out.println("\n");
+
+		Xbox x10 = new Xbox();
+		System.out.println("This is Tenth Xbox \n");
+
+		x10.model = "Series Z";
+		x10.processor = "AMD Zen 3";
+		x10.clockSpeed = 4.0;
+		x10.support4K = true;
+		x10.controllerType = "Wireless";
+		x10.ports = 4;
+		x10.powerConsumption = 350;
+		x10.origin = "USA";
+		x10.onlineSupport = true;
+		x10.edition = "Future";
+
+		System.out.println("Model :" + x10.model);
+		System.out.println("Processor :" + x10.processor);
+		System.out.println("Clock :" + x10.clockSpeed);
+		System.out.println("4K :" + x10.support4K);
+		System.out.println("Controller :" + x10.controllerType);
+		System.out.println("Ports :" + x10.ports);
+		System.out.println("Power :" + x10.powerConsumption);
+		System.out.println("Origin :" + x10.origin);
+		System.out.println("Online :" + x10.onlineSupport);
+		System.out.println("Edition :" + x10.edition);
+		System.out.println("\n");
+	}
+}
