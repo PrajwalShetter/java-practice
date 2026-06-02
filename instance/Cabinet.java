06083a6e021f1a27a046b945e1b2a0e963d23767
prajwal shetter <198955243+PrajwalShetter@users.noreply.github.com>
2026-06-02 23:22:14 +0530

    Learning instance

---
+++ instance/Cabinet.java
@@ -0,0 +1,19 @@
+class Cabinet{
+	
+	static String brand = "Godrej";
+	static String color = "Brown";
+	static double price = 8000;
+	static boolean isWood = true;
+	static int shelves = 4;
+	
+	String type;
+	String material;
+	double height;
+	double weight;
+	boolean lockable;
+	String shape;
+	int doors;
+	double width;
+	String origin;
+	boolean movable;
+}
