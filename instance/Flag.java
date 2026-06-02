9929f2947964c5a45ce06d167ee336d4275fe2eb
prajwal shetter <198955243+PrajwalShetter@users.noreply.github.com>
2026-06-02 23:23:49 +0530

    Learning instance

---
+++ instance/Flag.java
@@ -0,0 +1,19 @@
+/*
+Declare below classes, then declare explicit no-param const , then declare 5 instance variables . Create 5 instances , 
+then print the instance variables with default values , then update instance variables and then print the update values 
+Flag,Steel,Cap,Ipad,PowerBank,Flight,JoyStick,PhotoFrame,Cup,PlayStation
+*/
+
+class Flag {
+	
+	String country;
+	String color;
+	int stripes;
+	boolean hasSymbol;
+	double size;
+
+
+	public Flag(){
+		System.out.println("Instance created");
+	}
+}
