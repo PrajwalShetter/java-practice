f25b4337ae43a3791f8ddabb3319ee2349628d44
prajwal shetter <198955243+PrajwalShetter@users.noreply.github.com>
2026-06-06 23:51:18 +0530

    instance

---
+++ instance/Fire.java
@@ -0,0 +1,19 @@
+class Fire{
+	
+	static String typeName = "Combustion";
+	static String color = "Orange";
+	static double temperature = 800;
+	static boolean isDangerous = true;
+	static int intensity = 5;
+	
+	String source;
+	String fuel;
+	double duration;
+	boolean smoke;
+	String location;
+	int spreadLevel;
+	double heatOutput;
+	String origin;
+	boolean controllable;
+	String shape;
+}
