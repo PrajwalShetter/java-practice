6d18a8845611e3dec01b4c097f4f1ddd86eae8d5
prajwal shetter <198955243+PrajwalShetter@users.noreply.github.com>
2026-06-06 23:49:57 +0530

    instance

---
+++ instance/Ethernet.java
@@ -0,0 +1,18 @@
+class Ethernet {	
+	static String type = "LAN";
+	static String cableType = "Cat6";
+	static double maxSpeed = 1000;
+	static boolean wired = true;
+	static int standard = 802;
+	
+	String connector;
+	String length;
+	double bandwidth;
+	boolean shielded;
+	String usage;
+	int ports;
+	double latency;
+	String origin;
+	boolean secure;
+	String compatibility;
+}
