209559162b2ed6dd74fc2ce85df11c1dea78007c
prajwal shetter <198955243+PrajwalShetter@users.noreply.github.com>
2026-06-07 22:55:13 +0530

    instance

---
+++ instance/Mouse.java
@@ -0,0 +1,19 @@
+class Mouse{
+	
+	static String brand = "Logitech";
+	static String color = "Black";
+	static double price = 999;
+	static boolean wireless = true;
+	static int dpi = 1600;
+	
+	String type;
+	String connectivity;
+	double weight;
+	boolean gaming;
+	String shape;
+	int buttons;
+	double batteryLife;
+	String origin;
+	boolean rgb;
+	String compatibility;
+}
