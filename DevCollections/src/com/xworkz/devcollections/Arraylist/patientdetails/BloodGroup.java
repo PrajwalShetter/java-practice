590ec1b214c5a0d87eda60d60d3e3194a1c0d2d7
prajwal shetter <198955243+PrajwalShetter@users.noreply.github.com>
2026-06-30 22:14:31 +0530

    Peeforming operation on ArrayList

---
+++ DevCollections/src/com/xworkz/devcollections/Arraylist/patientdetails/BloodGroup.java
@@ -0,0 +1,6 @@
+package com.xworkz.devcollections.Arraylist.patientdetails;
+
+public enum BloodGroup {
+
+    APOSITIVE, ANEGATIVE, BPOSITIVE, BNEGATIVE, OPOSITIVE, ONEGATIVE, BOMBYBLOODGROPU,ABPOSITIVE, ABNEGATIVE;
+}
