3aa73ce04dd9f09d1cebfd53279a16b4e16ae11c
prajwal shetter <198955243+PrajwalShetter@users.noreply.github.com>
2026-07-01 23:31:05 +0530

    Performing operation on LinkedList

---
+++ DevCollections/src/com/xworkz/devcollections/linkedlist/countryInfo/constant/Continent.java
@@ -0,0 +1,8 @@
+package com.xworkz.devcollections.linkedlist.countryInfo.constant;
+
+public enum Continent {
+
+
+        ASIA,AFRICA, NORTH_AMERICA , SOUTH_AMERICA,EUROPE , AUSTRALIA,ANTARCTICA;
+    }
+
