373d5179363d361314cfb120c79c275c1e191496
prajwal shetter <198955243+PrajwalShetter@users.noreply.github.com>
2026-07-12 23:41:44 +0530

    stored data in linked list and retriving data using methods

---
+++ DevCollections/src/com/xworkz/devcollections/linkedlist/countryInfo/dao/CountryDao.java
@@ -0,0 +1,26 @@
+package com.xworkz.devcollections.linkedlist.countryInfo.dao;
+
+import com.xworkz.devcollections.linkedlist.countryInfo.constant.Continent;
+import com.xworkz.devcollections.linkedlist.countryInfo.dto.CountryDto;
+
+import java.util.List;
+
+public interface CountryDao {
+
+    void add(CountryDto country);
+
+    void getAllCountryInfo();
+
+    void getCountriesByContinent(Continent continent);
+
+    void getNoOfStatesByCountryName(String countryName);
+
+    double getAreaByCountryName(String countryName);
+
+    String getCountryCodeByCountryName(String countryName);
+
+    String getPrimeMinisterNameByCountryName(String countryName);
+
+    boolean updatePollutionByCountryCode(String countryCode);
+
+}
