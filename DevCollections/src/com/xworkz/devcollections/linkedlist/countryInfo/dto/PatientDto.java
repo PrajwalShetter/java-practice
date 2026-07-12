5e3d0669d57a017e79b0c6573f86e35a89efc6ea
prajwal shetter <198955243+PrajwalShetter@users.noreply.github.com>
2026-07-12 23:42:14 +0530

    stored data in linked list and retriving data using methods

---
+++ DevCollections/src/com/xworkz/devcollections/linkedlist/countryInfo/dto/PatientDto.java
@@ -0,0 +1,18 @@
+package com.xworkz.devcollections.linkedlist.countryInfo.dto;
+
+import lombok.*;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+
+
+public class PatientDto {
+
+    private String name ;
+    private int Id;
+    private String address;
+    private int age;
+}
