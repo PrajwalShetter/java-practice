1d4ed19d0fa954b43760d91ca38b607c20dee9ac
prajwal shetter <198955243+PrajwalShetter@users.noreply.github.com>
2026-08-05 23:51:53 +0530

    Demonostrating the student managment system on stream

---
+++ DevCollections/src/com/xworkz/devcollections/Arraylist/studentManagement/Dto/StudentDto.java
@@ -0,0 +1,18 @@
+package com.xworkz.devcollections.Arraylist.studentManagement.Dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class StudentDto {
+
+    private int id;
+    private String name;
+    private double percentage;
+
+
+}
